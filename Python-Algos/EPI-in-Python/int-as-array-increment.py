b1deb6a83c6c369e245003424dc6bf00419c9f65
josephozgen <josephozgen@gmail.com>
2020-08-11 01:01:04 -0700

    solved

---
+++ Python-Algos/EPI-in-Python/int-as-array-increment.py
@@ -0,0 +1,19 @@
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
+
+arr = [10, 5, 8]
+#print(plus_one(arr))
+
+# The time complexity is O(n).
+
+
+
